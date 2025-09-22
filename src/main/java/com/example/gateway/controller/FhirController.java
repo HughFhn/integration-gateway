7b9f4eea457a6ec458819766a0e3c65467590903
Hugh <hughfeehan22@gmail.com>
2025-09-22 13:57:16 +0100

    Added auditing and write fhir conversion to output file

--- src/main/java/com/example/gateway/controller/FhirController.java
+++ src/main/java/com/example/gateway/controller/FhirController.java
@@ -1,6 +1,8 @@
 package com.example.gateway.controller;
 
+import com.example.gateway.converter.FhirToHl7Converter;
 import com.example.gateway.converter.Hl7ToFhirConverter;
+import org.hl7.fhir.r4.model.Patient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -22,7 +24,8 @@ public class FhirController {
 
     private final FhirContext fhirContext = FhirContext.forR4();
 
-    @PostMapping("/convert")
+    // Convert Hl7 to Fhir Json
+    @PostMapping("/convert/hl7-to-fhir")
     public ResponseEntity<String> convertHl7ToFhir(@RequestBody String hl7) throws Exception {
         System.out.println("\nReceived HL7:\n" + hl7); // Debug print
 
@@ -54,8 +57,48 @@ public class FhirController {
 
         // Print to show conversion is done
         System.out.println("\nConversion complete!");
-        System.out.println("Converted HL7: "+fhirJson);
+        System.out.println("Converted HL7 to Fhir: "+fhirJson);
 
         return ResponseEntity.ok(fhirJson);
+    }
+    // Convert Fhir Json -> Hl7
+    @PostMapping("/convert/fhir-to-hl7")
+    public ResponseEntity<String> convertFhirToHl7(@RequestBody String fhirJson) throws Exception {
+        System.out.println("\nReceived Fhir Json:\n" + fhirJson); // Debug print
+
+        var resource = fhirContext.newJsonParser().parseResource(fhirJson);
+        // Confirm that its just Patient and admission so far
+        if (!(resource instanceof Patient)) {
+            throw new IllegalArgumentException("Only Patient resource supported for now.\nMust implement others if needed");
+        }
+        // Try conversion, if not print stack trace
+        String hl7Message = null;
+        try {
+            hl7Message = FhirToHl7Converter.convertFhirToPatient((Patient) resource);
+            System.out.println("Converted Fhir Json: " + hl7Message);
+        } catch (Exception e) {
+            System.out.println("FhirToHl7Converter failed");
+            System.out.println(e);
+        }
+
+        String outputPath = "output/test-hl7.hl7";
+        // Date object for audit log
+        Date dateNow = new Date();
+
+        // Write conversion output
+        try (FileWriter fw = new FileWriter(outputPath)) {
+            fw.write(hl7Message);
+        }
+
+        // Write audit log (append mode so you don’t overwrite)
+        try (FileWriter auditWrite = new FileWriter(outputPath, true)) {
+            auditWrite.write("\nConversion performed at " + dateNow);
+        }
+
+        // Print to show conversion is done
+        System.out.println("\nConversion complete!");
+        System.out.println("Converted Fhir to Hl7: "+hl7Message);
+
+        return ResponseEntity.ok(hl7Message);
     }
 }
